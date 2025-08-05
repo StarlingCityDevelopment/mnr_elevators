2ee588ad411ebba52d50b799abde58f350f81b9a
Codexis <contact@codexis.dev>
2025-08-05 16:45:02 +0200

    refactor: configuration

--- config/config.lua
+++ config/config.lua
@@ -1,33 +1,33 @@
 return {
     ["fib"] = {
-        label = "FIB Headquarter",
+        label = "Siège du FIB",
         debug = false,
         floors = {
             [1] = {
-                title = "Ground Floor",
-                description = "Entrance and Desk",
+                title = "Rez-de-chaussée",
+                description = "Entrée et bureau d'accueil",
                 zones = {
-                    [1] = {coords = vec4(136.0807, -761.7469, 46.00, 160.00), size = vec3(2.0, 2.0, 2.5)}
+                    [1] = { coords = vec4(136.0807, -761.7469, 46.00, 160.00), size = vec3(2.0, 2.0, 2.5) }
                 },
                 locked = false,
                 item = false,
                 jobs = false,
             },
             [2] = {
-                title = "Floor 47",
-                description = "Legal Affairs",
+                title = "Étage 47",
+                description = "Affaires juridiques",
                 zones = {
-                    [1] = {coords = vec4(136.0939, -761.8068, 234.50, 160.00), size = vec3(2.0, 2.0, 2.5)}
+                    [1] = { coords = vec4(136.0939, -761.8068, 234.50, 160.00), size = vec3(2.0, 2.0, 2.5) }
                 },
                 locked = false,
                 item = false,
                 jobs = false,
             },
             [3] = {
-                title = "Floor 49",
+                title = "Étage 49",
                 description = "Communications",
                 zones = {
-                    [1] = {coords = vec4(136.0755, -761.7881, 242.50, 160.00), size = vec3(2.0, 2.0, 2.5)}
+                    [1] = { coords = vec4(136.0755, -761.7881, 242.50, 160.00), size = vec3(2.0, 2.0, 2.5) }
                 },
                 locked = false,
                 item = false,
@@ -35,7 +35,287 @@ return {
             }
         },
     },
-    ---@description Paste the presets here 🔽 [https://github.com/Monarch-Development/Monarch-Presets/tree/main/elevators]
 
-    ---@description Paste the presets here 🔼 [https://github.com/Monarch-Development/Monarch-Presets/tree/main/elevators]
+    ["dailyglobe"] = {
+        label = "Daily Globe",
+        debug = false,
+        floors = {
+            [1] = {
+                title = "Rez-de-chaussée",
+                description = "Entrée et bureau d'accueil",
+                zones = {
+                    [1] = { coords = vec4(136.0807, -761.7469, 46.00, 160.00), size = vec3(2.0, 2.0, 2.5) }
+                },
+                locked = false,
+                item = false,
+                jobs = "dailyglobe",
+            },
+            [2] = {
+                title = "Garage",
+                description = "Accès réservé aux employés",
+                zones = {
+                    [1] = { coords = vec4(136.0939, -761.8068, 234.50, 160.00), size = vec3(2.0, 2.0, 2.5) }
+                },
+                locked = false,
+                item = false,
+                jobs = "dailyglobe",
+            },
+        },
+    },
+
+    ["central-ls-medical-center"] = {
+        label = "Centre Médical Central LS",
+        debug = false,
+        floors = {
+            [1] = {
+                title = "Étage 1",
+                description = "Salle de réunion et chambres des patients",
+                zones = {
+                    [1] = { coords = vec4(362.6882, -1407.4971, 38.60, 320.00), size = vec3(2.0, 2.0, 2.3) },
+                    [2] = { coords = vec4(355.5820, -1415.9916, 38.60, 140.00), size = vec3(2.0, 2.0, 2.3) },
+                },
+                locked = false,
+                item = false,
+                jobs = false,
+            },
+            [2] = {
+                title = "Rez-de-chaussée",
+                description = "Bureau d'accueil et salles d'urgence",
+                zones = {
+                    [1] = { coords = vec4(362.6882, -1407.4971, 33.20, 320.00), size = vec3(2.0, 2.0, 2.3) },
+                    [2] = { coords = vec4(355.5820, -1415.9916, 33.20, 140.00), size = vec3(2.0, 2.0, 2.3) },
+                },
+                locked = false,
+                item = false,
+                jobs = false,
+            },
+        },
+    },
+
+    ["mile-high-club"] = {
+        label = "Mile High Club",
+        debug = false,
+        floors = {
+            [1] = {
+                title = "Toit",
+                description = "Héliport",
+                zones = {
+                    [1] = { coords = vec4(-162.8081, -979.9983, 270.20, 340.00), size = vec3(2.1, 2.1, 2.3) },
+                },
+                locked = false,
+                item = false,
+                jobs = false,
+            },
+            [2] = {
+                title = "Bureau du président",
+                description = "",
+                zones = {
+                    [1] = { coords = vec4(-150.3333, -984.4547, 265.00, 160.00), size = vec3(2.1, 2.1, 2.3) },
+                    [2] = { coords = vec4(-151.4568, -987.3717, 265.00, 340.00), size = vec3(2.1, 2.1, 2.3) },
+                    [3] = { coords = vec4(-155.0241, -982.7457, 265.00, 160.00), size = vec3(2.1, 2.1, 2.3) },
+                    [4] = { coords = vec4(-156.1082, -985.6997, 265.00, 340.00), size = vec3(2.1, 2.1, 2.3) },
+                },
+                locked = false,
+                item = false,
+                jobs = false,
+            },
+            [3] = {
+                title = "Assmosphere",
+                description = "",
+                zones = {
+                    [1] = { coords = vec4(-150.3333, -984.4547, 254.60, 160.00), size = vec3(2.1, 2.1, 2.3) },
+                    [2] = { coords = vec4(-151.4568, -987.3717, 254.60, 340.00), size = vec3(2.1, 2.1, 2.3) },
+                    [3] = { coords = vec4(-155.0241, -982.7457, 254.60, 160.00), size = vec3(2.1, 2.1, 2.3) },
+                    [4] = { coords = vec4(-156.1082, -985.6997, 254.60, 340.00), size = vec3(2.1, 2.1, 2.3) },
+                },
+                locked = false,
+                item = false,
+                jobs = false,
+            },
+            [4] = {
+                title = "Chambres Étage 6",
+                description = "",
+                zones = {
+                    [1] = { coords = vec4(-150.3333, -984.4547, 212.60, 160.00), size = vec3(2.1, 2.1, 2.3) },
+                    [2] = { coords = vec4(-151.4568, -987.3717, 212.60, 340.00), size = vec3(2.1, 2.1, 2.3) },
+                    [3] = { coords = vec4(-155.0241, -982.7457, 212.60, 160.00), size = vec3(2.1, 2.1, 2.3) },
+                    [4] = { coords = vec4(-156.1082, -985.6997, 212.60, 340.00), size = vec3(2.1, 2.1, 2.3) },
+                },
+                locked = false,
+                item = false,
+                jobs = false,
+            },
+            [5] = {
+                title = "Chambres Étage 5",
+                description = "",
+                zones = {
+                    [1] = { coords = vec4(-150.3333, -984.4547, 206.40, 160.00), size = vec3(2.1, 2.1, 2.3) },
+                    [2] = { coords = vec4(-151.4568, -987.3717, 206.40, 340.00), size = vec3(2.1, 2.1, 2.3) },
+                    [3] = { coords = vec4(-155.0241, -982.7457, 206.40, 160.00), size = vec3(2.1, 2.1, 2.3) },
+                    [4] = { coords = vec4(-156.1082, -985.6997, 206.40, 340.00), size = vec3(2.1, 2.1, 2.3) },
+                },
+                locked = false,
+                item = false,
+                jobs = false,
+            },
+            [6] = {
+                title = "Chambres Étage 4",
+                description = "",
+                zones = {
+                    [1] = { coords = vec4(-150.3333, -984.4547, 200.00, 160.00), size = vec3(2.1, 2.1, 2.3) },
+                    [2] = { coords = vec4(-151.4568, -987.3717, 200.00, 340.00), size = vec3(2.1, 2.1, 2.3) },
+                    [3] = { coords = vec4(-155.0241, -982.7457, 200.00, 160.00), size = vec3(2.1, 2.1, 2.3) },
+                    [4] = { coords = vec4(-156.1082, -985.6997, 200.00, 340.00), size = vec3(2.1, 2.1, 2.3) },
+                },
+                locked = false,
+                item = false,
+                jobs = false,
+            },
+            [7] = {
+                title = "Chambres Étage 3",
+                description = "",
+                zones = {
+                    [1] = { coords = vec4(-150.3333, -984.4547, 193.90, 160.00), size = vec3(2.1, 2.1, 2.3) },
+                    [2] = { coords = vec4(-151.4568, -987.3717, 193.90, 340.00), size = vec3(2.1, 2.1, 2.3) },
+                    [3] = { coords = vec4(-155.0241, -982.7457, 193.90, 160.00), size = vec3(2.1, 2.1, 2.3) },
+                    [4] = { coords = vec4(-156.1082, -985.6997, 193.90, 340.00), size = vec3(2.1, 2.1, 2.3) },
+                },
+                locked = false,
+                item = false,
+                jobs = false,
+            },
+            [8] = {
+                title = "Chambres Étage 2",
+                description = "",
+                zones = {
+                    [1] = { coords = vec4(-150.3333, -984.4547, 187.70, 160.00), size = vec3(2.1, 2.1, 2.3) },
+                    [2] = { coords = vec4(-151.4568, -987.3717, 187.70, 340.00), size = vec3(2.1, 2.1, 2.3) },
+                    [3] = { coords = vec4(-155.0241, -982.7457, 187.70, 160.00), size = vec3(2.1, 2.1, 2.3) },
+                    [4] = { coords = vec4(-156.1082, -985.6997, 187.70, 340.00), size = vec3(2.1, 2.1, 2.3) },
+                },
+                locked = false,
+                item = false,
+                jobs = false,
+            },
+            [9] = {
+                title = "Chambres Étage 1",
+                description = "",
+                zones = {
+                    [1] = { coords = vec4(-150.3333, -984.4547, 181.30, 160.00), size = vec3(2.1, 2.1, 2.3) },
+                    [2] = { coords = vec4(-151.4568, -987.3717, 181.30, 340.00), size = vec3(2.1, 2.1, 2.3) },
+                    [3] = { coords = vec4(-155.0241, -982.7457, 181.30, 160.00), size = vec3(2.1, 2.1, 2.3) },
+                    [4] = { coords = vec4(-156.1082, -985.6997, 181.30, 340.00), size = vec3(2.1, 2.1, 2.3) },
+                },
+                locked = false,
+                item = false,
+                jobs = false,
+            },
+            [10] = {
+                title = "SPA & Gym",
+                description = "",
+                zones = {
+                    [1] = { coords = vec4(-150.3333, -984.4547, 175.00, 160.00), size = vec3(2.1, 2.1, 2.3) },
+                    [2] = { coords = vec4(-151.4568, -987.3717, 175.00, 340.00), size = vec3(2.1, 2.1, 2.3) },
+                    [3] = { coords = vec4(-155.0241, -982.7457, 175.00, 160.00), size = vec3(2.1, 2.1, 2.3) },
+                    [4] = { coords = vec4(-156.1082, -985.6997, 175.00, 340.00), size = vec3(2.1, 2.1, 2.3) },
+                },
+                locked = false,
+                item = false,
+                jobs = false,
+            },
+            [11] = {
+                title = "Piscine sur le toit",
+                description = "Réservée aux clients de l'hôtel",
+                zones = {
+                    [1] = { coords = vec4(-124.5557, -975.0193, 115.60, 250.00), size = vec3(2.1, 2.1, 2.3) },
+                },
+                locked = false,
+                item = false,
+                jobs = false,
+            },
+            [12] = {
+                title = "Piscine sur le toit",
+                description = "Accessible publiquement",
+                zones = {
+                    [1] = { coords = vec4(-150.3333, -984.4547, 53.30, 160.00), size = vec3(2.1, 2.1, 2.3) },
+                    [2] = { coords = vec4(-151.4568, -987.3717, 53.30, 340.00), size = vec3(2.1, 2.1, 2.3) },
+                    [3] = { coords = vec4(-155.0241, -982.7457, 53.30, 160.00), size = vec3(2.1, 2.1, 2.3) },
+                    [4] = { coords = vec4(-156.1082, -985.6997, 53.30, 340.00), size = vec3(2.1, 2.1, 2.3) },
+                },
+                locked = false,
+                item = false,
+                jobs = false,
+            },
+            [13] = {
+                title = "Salon de Cigares",
+                description = "",
+                zones = {
+                    [1] = { coords = vec4(-150.3333, -984.4547, 46.00, 160.00), size = vec3(2.1, 2.1, 2.3) },
+                    [2] = { coords = vec4(-151.4568, -987.3717, 46.00, 340.00), size = vec3(2.1, 2.1, 2.3) },
+                    [3] = { coords = vec4(-155.0241, -982.7457, 46.00, 160.00), size = vec3(2.1, 2.1, 2.3) },
+                    [4] = { coords = vec4(-156.1082, -985.6997, 46.00, 340.00), size = vec3(2.1, 2.1, 2.3) },
+                },
+                locked = false,
+                item = false,
+                jobs = false,
+            },
+            [14] = {
+                title = "Restaurant Français",
+                description = "",
+                zones = {
+                    [1] = { coords = vec4(-150.3333, -984.4547, 39.50, 160.00), size = vec3(2.1, 2.1, 2.3) },
+                    [2] = { coords = vec4(-151.4568, -987.3717, 39.50, 340.00), size = vec3(2.1, 2.1, 2.3) },
+                    [3] = { coords = vec4(-155.0241, -982.7457, 39.50, 160.00), size = vec3(2.1, 2.1, 2.3) },
+                    [4] = { coords = vec4(-156.1082, -985.6997, 39.50, 340.00), size = vec3(2.3, 2.3, 2.3) },
+                },
+                locked = false,
+                item = false,
+                jobs = false,
+            },
+            [15] = {
+                title = "Hall",
+                description = "",
+                zones = {
+                    [1] = { coords = vec4(-150.3333, -984.4547, 29.60, 160.00), size = vec3(2.1, 2.1, 2.3) },
+                    [2] = { coords = vec4(-151.4568, -987.3717, 29.60, 340.00), size = vec3(2.1, 2.1, 2.3) },
+                    [3] = { coords = vec4(-155.0241, -982.7457, 29.60, 160.00), size = vec3(2.1, 2.1, 2.3) },
+                    [4] = { coords = vec4(-156.1082, -985.6997, 29.60, 340.00), size = vec3(2.1, 2.1, 2.3) },
+                    [5] = { coords = vec4(-135.5909, -984.3957, 29.60, 160.00), size = vec3(2.0, 2.0, 2.3) },
+                },
+                locked = false,
+                item = false,
+                jobs = false,
+            },
+            [16] = {
+                title = "Garage Étage -1",
+                description = "",
+                zones = {
+                    [1] = { coords = vec4(-135.4880, -984.3469, 23.30, 160.00), size = vec3(2.0, 2.0, 2.3) },
+                },
+                locked = false,
+                item = false,
+                jobs = false,
+            },
+            [17] = {
+                title = "Garage Étage -2",
+                description = "",
+                zones = {
+                    [1] = { coords = vec4(-135.4880, -984.3469, 18.20, 160.00), size = vec3(2.0, 2.0, 2.3) },
+                },
+                locked = false,
+                item = false,
+                jobs = false,
+            },
+            [18] = {
+                title = "Garage Étage -3",
+                description = "",
+                zones = {
+                    [1] = { coords = vec4(-135.4880, -984.3469, 13.20, 160.00), size = vec3(2.0, 2.0, 2.3) },
+                },
+                locked = false,
+                item = false,
+                jobs = false,
+            },
+        },
+    },
 }
